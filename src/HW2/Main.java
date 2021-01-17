ff74c32fd98725a42eece6ab2f32967f6669bcd1
Pa1nK1ller <Kostya.300397@gmail.com>
2021-01-17 14:28:43 +0300

    Выполненное 1 дз

--- src/HW2/Main.java
+++ src/HW2/Main.java
@@ -2,63 +2,51 @@ package HW2;
 
 public class Main {
     public static void main(String[] args) {
-        int res = 0;
         String[][] arr = {{"1", "2", "3", "4"}, {"4", "3", "2", "1"}};
         String[][] arr2 = {{"1", "2", "3", "4", "5", "6"}, {"1", "2", "3", "4"}};
         String[][] arr3 = {{"1", "2", "3", "4"}, {"1", "2", "зз", "4"}};
+        String[][] arr4 = new String[][]{{"3", "3", "3", "3"}, {"3", "3", "3", "3"}, {"3", "3", "3", "3"}, {}};
 
         try {
-            res = check(arr);
-        } catch (MyArraySizeException e) {
+            System.out.println("сумма элементов массива = " + check(arr));
+        } catch (MyArraySizeException | MyArrayDataException e) {
             System.out.println(e.getMessage());
-        } catch (MyArrayDataException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("сумма элементов массива = " + String.valueOf(res));
-
         }
         try {
-            res = 0;
-            res = check(arr2);
-        } catch (MyArraySizeException e) {
+            System.out.println("сумма элементов массива = " + check(arr2));
+        } catch (MyArraySizeException | MyArrayDataException e) {
             System.out.println(e.getMessage());
-        } catch (MyArrayDataException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("сумма элементов массива = " + String.valueOf(res));
-
         }
         try {
-            res = 0;
-            res = check(arr3);
-        } catch (MyArraySizeException e) {
+            System.out.println("сумма элементов массива = " + check(arr3));
+        } catch (MyArraySizeException | MyArrayDataException e) {
             System.out.println(e.getMessage());
-        } catch (MyArrayDataException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("сумма элементов массива = " + String.valueOf(res));
-
         }
+        try {
+            System.out.println("сумма элементов массива = " + check(arr4));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            System.out.println("сумма элементов массива = " + check(new String[][]{{"3", "3", "3", "3"}, {"3", "3", "3", "3"}, {"3", "3", "3", "3"}, {}}));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+
 
     }
 
     public static int check(String[][] array) throws MyArraySizeException, MyArrayDataException {
         int sum = 0;
-        int value;
-        int row ;
-        int cell;
         if (array.length != 2 || array[0].length != 4 || array[1].length != 4) {
             throw new MyArraySizeException();
         }
-        for (int i = 1; i < 3; i++) {
-            row = i;
-            for (int c = 1; c < 5; c++) {
-                cell = c;
+        for (int i = 0; i < array.length; i++) {
+            for (int c = 0; c < array[i].length; c++) {
                 try {
-                    value = Integer.parseInt(array[i - 1][c - 1]);
-                    sum += value;
+                    sum += Integer.parseInt(array[i][c]);
                 } catch (IllegalArgumentException e) {
-                    String message = "в " + String.valueOf(row) + " ряду, " + String.valueOf(cell) + " ячейке";
+                    String message = "в " + (i + 1) + " ряду, " + (c + 1) + " ячейке";
                     throw new MyArrayDataException(message);
                 }
             }
